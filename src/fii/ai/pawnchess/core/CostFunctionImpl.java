ed2a7a8c3d18ca8c4e0cd550a05273e74d3f8621
CristiHuma96 <cristihuma96@gmail.com>
2017-11-08 15:42:14 +0200

    Tree implementation and work started on a cost function implementation

---
+++ src/fii/ai/pawnchess/core/CostFunctionImpl.java
@@ -0,0 +1,14 @@
+package fii.ai.pawnchess.core;
+
+public class CostFunctionImpl implements CostFunction
+{
+    @Override
+    public double computeCost(State state)
+    {
+        return 0;
+    }
+
+    private int howManyStepsBeforeFinalPosition(Position position){
+        return 0;
+    }
+}
