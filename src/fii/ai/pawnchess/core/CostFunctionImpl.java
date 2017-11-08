45d2f93797c92285dc80002591440be5842ab861
CristiHuma96 <cristihuma96@gmail.com>
2017-11-08 21:39:19 +0200

    Update on costFunction, still needs some work

--- src/fii/ai/pawnchess/core/CostFunctionImpl.java
+++ src/fii/ai/pawnchess/core/CostFunctionImpl.java
@@ -7,16 +7,22 @@ import java.util.stream.Collectors;
 
 public class CostFunctionImpl implements CostFunction
 {
+    private List<Position> whitePositions;
+    private List<Position> blackPositions;
+
     @Override
     public double computeCost(State state)
     {
-        return this.howManyStepsBeforeFinalPosition(state);
+        this.whitePositions = state.getPiecesPositionsFor(PlayerColor.WHITE);
+        this.blackPositions = state.getPiecesPositionsFor(PlayerColor.BLACK);
+        double result = 0;
+//        result += this.howManyStepsBeforeFinalPosition(state)*0.2;
+        result += this.pawnStructure(state)*0.8;
+        return result;
     }
 
     private int howManyStepsBeforeFinalPosition(State state)
     {
-        List<Position> whitePositions = state.getPiecesPositionsFor(PlayerColor.WHITE);
-        List<Position> blackPositions = state.getPiecesPositionsFor(PlayerColor.BLACK);
         List<Position> blacks;
         List<Position> whites;
         Optional<Position> blackPosition;
@@ -26,19 +32,47 @@ public class CostFunctionImpl implements CostFunction
         {
             int finalJ = j;
 
-            blacks = blackPositions.stream().filter(x -> x.getColumn() == finalJ).collect(Collectors.toList());
+            blacks = this.blackPositions.stream().filter(x -> x.getColumn() == finalJ).collect(Collectors.toList());
             blackPosition = blacks.stream().max(Comparator.comparing(Position::getRow));
 
-            whites = whitePositions.stream().filter(x -> x.getColumn() == finalJ).collect(Collectors.toList());
+            whites = this.whitePositions.stream().filter(x -> x.getColumn() == finalJ).collect(Collectors.toList());
             whitePosition = whites.stream().max(Comparator.comparing(Position::getRow));
 
-            if(blackPosition.isPresent()){
-                blackPositions.remove(blackPosition.get());
-                if(whitePosition.isPresent()){
-                    if(whitePosition.get().getRow() < blackPosition.get().getRow())
-                        result -= whitePosition.get().getRow() * 10 * whitePosition.get().getRow();
+            if (blackPosition.isPresent())
+            {
+                this.blackPositions.remove(blackPosition.get());
+                if (whitePosition.isPresent())
+                {
+                    if (whitePosition.get().getRow() < blackPosition.get().getRow())
+                        result -= whitePosition.get().getRow() * 2 * whitePosition.get().getRow();
                 }
-                result +=  (8 - blackPosition.get().getRow()) *10;
+                result += (8 - blackPosition.get().getRow()) * 2;
+            }
+        }
+        return result;
+    }
+
+    private int pawnStructure(State state)
+    {
+        int result = 0;
+        Position leftNeighbour = new Position(-1, -1);
+        Position rightNeighbour = new Position(-1, -1);
+        for (Position position : blackPositions)
+        {
+            leftNeighbour.setRow(position.getRow() - 1);
+            rightNeighbour.setRow(position.getRow() - 1);
+
+            leftNeighbour.setColumn(position.getRow() - 1);
+            rightNeighbour.setColumn(position.getColumn() + 1);
+            if (state.hasPieceOnPosition(leftNeighbour, PlayerColor.BLACK)
+                    && state.hasPieceOnPosition(rightNeighbour, PlayerColor.BLACK))
+                result += 300;
+            else
+            {
+                if (state.hasPieceOnPosition(leftNeighbour, PlayerColor.WHITE))
+                result += 100;
+                if (state.hasPieceOnPosition(rightNeighbour, PlayerColor.BLACK))
+                    result += 100;
             }
         }
         return result;
